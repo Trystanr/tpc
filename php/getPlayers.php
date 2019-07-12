887d1f7865903285ba4f63cb4073754c1d0b1831
Trystan <trystan.b@hotmail.com>
2019-07-12 10:19:03 +0200

    Update getPlayers.php

--- php/getPlayers.php
+++ php/getPlayers.php
@@ -1,5 +1,8 @@
 <?php
+	// required headers
+	header("Access-Control-Allow-Origin: *");
 	header("Content-Type: application/json; charset=UTF-8");
+ 
 
 	error_reporting(E_ALL);
 	ini_set('display_errors', 1);
